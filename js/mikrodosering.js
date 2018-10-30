ef4a471251699ffc91d0858ba544df4a705414ae
Vegard <vsjoen@gmail.com>
2018-10-30 16:14:32 +0100

    Fixed several small bugs. Reduced spacing on smaller screen sizes.

--- js/mikrodosering.js
+++ js/mikrodosering.js
@@ -8,7 +8,7 @@
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: (target.offset().top - 57)
+          scrollTop: (target.offset().top - navOffset)
         }, 1000, "easeInOutExpo");
         return false;
       }
@@ -26,6 +26,16 @@
     offset: 57 + 1
   });
 
+  var navOffset;
+  var setOffset = function() {
+      if ($(window).width() < 960) {
+        navOffset = 57;
+      }
+      else {
+        navOffset = 57;
+      }
+  };
+
   // Collapse Navbar
   var navbarCollapse = function() {
     if ($("#mainNav").offset().top > 100) {
@@ -34,10 +44,34 @@
       $("#mainNav").removeClass("navbar-shrink");
     }
   };
+
+  // Scroll Navbar
+  var navbarScroll = function() {
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+      var currentScrollPos = window.pageYOffset;
+      if (prevScrollpos > currentScrollPos) {
+        document.getElementById("mainNav").style.top = "0";
+      } else if ($(window).width() < 960) {
+        document.getElementById("mainNav").style.top = "-70px";
+      }
+      prevScrollpos = currentScrollPos;
+    }
+  };
+
+  // Set offset
+  setOffset();
+
+  // Hide nav when scrolling down on small screens
+  //navbarScroll();
+
   // Collapse now if page is not at top
   navbarCollapse();
+
   // Collapse the navbar when page is scrolled
-$(window).scroll(navbarCollapse);
+  $(window).scroll(navbarCollapse);
+  $(window).resize(setOffset);
+
   // Scroll reveal calls
   window.sr = ScrollReveal();
   sr.reveal('.sr-warning', {
